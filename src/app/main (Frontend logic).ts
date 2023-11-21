a90bee36c515f81c97f602cff23ecae1d0f1b584
PabloWilkano <pablowilkano513@gmail.com>
2023-11-21 04:57:34 -0600

    Add files via upload

---
+++ src/app/main (Frontend logic).ts
@@ -0,0 +1,14 @@
+// RITA-project/src/modules/index.ts
+
+import { startServer } from '../server/server';
+import { initializeUI } from '../app/index';
+import { initializeAppLogic } from './appLogic';
+
+// Initialize your application logic
+initializeAppLogic();
+
+// Start the Node.js server
+startServer();
+
+// Initialize the UI logic
+initializeUI();
