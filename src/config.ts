08c3a19005feb007f3da168019ae43b06855177d
PabloWilkano <pablowilkano513@gmail.com>
2023-11-21 12:31:58 -0600

    Create config.ts
    Will be gone soooooooooooon

---
+++ src/config.ts
@@ -0,0 +1,11 @@
+// config.ts
+
+// find a more secure way to handle secrets
+
+// OpenAI API Configuration
+export const openaiApiKey = 'your_openai_api_key_here';
+
+// Google OAuth Configuration
+export const googleOAuthClientId = 'your_google_oauth_client_id_here';
+export const googleOAuthClientSecret = 'your_google_oauth_client_secret_here';
+export const googleOAuthCallbackURL = 'http://your-app-callback-url-here';
