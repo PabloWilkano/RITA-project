a3ba1171abe6e1151a97c4d7c4747103279feba2
PabloWilkano <pablowilkano513@gmail.com>
2023-11-21 05:06:57 -0600

    Add files via upload

---
+++ config.ts
@@ -0,0 +1,14 @@
+// config.ts
+
+// find a more secure way to handle secrets
+
+// OpenAI API Configuration
+export const openaiApiKey = 'your_openai_api_key_here';
+
+// Google Voice to Text API Configuration
+export const googleVoiceToTextApiKey = 'your_google_voice_to_text_api_key_here';
+
+// Google OAuth Configuration
+export const googleOAuthClientId = 'your_google_oauth_client_id_here';
+export const googleOAuthClientSecret = 'your_google_oauth_client_secret_here';
+export const googleOAuthCallbackURL = 'http://your-app-callback-url-here';
